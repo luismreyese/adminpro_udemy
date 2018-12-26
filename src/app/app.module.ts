5197849143cc6cee08125381f53c835db3ea6fe2
luisR <luismreyese@gmail.com>
2018-12-25 23:58:26 -0500

    Modificación de rutas Plantilla Maestra V1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,13 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PagesModule } from './pages/pages.module';
 
 // Components
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NofoundpageComponent } from './shared/nofoundpage/nofoundpage.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { RegisterComponent } from './login/register.component';
 
 
 // Services
@@ -24,26 +19,15 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
 // Routers
 
 import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register.component';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NofoundpageComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
+    PagesModule,
     BrowserModule,
     APP_ROUTES
   ],
